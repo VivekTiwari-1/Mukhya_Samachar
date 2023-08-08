59bf875d46d3ddd467e61fa694d8c613b58408ce
Vivek Tiwari <vivektiwarii4545@gmail.com>
2023-08-08 23:29:37 +0530

    Added different categories of news

--- src/App.js
+++ src/App.js
@@ -5,12 +5,31 @@ import React, { Component } from 'react'
 import Navbar from './Components/Navbar';
 import News from './Components/News';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar/>
-        <News/>
+        <Router>
+          <Navbar />
+
+          <Routes>
+            <Route exact path="/" element={<News key="general" pagesize={20} country={'in'} category={'general'} />}/>
+            {/* <Route exact path="/business"><News key="business" pagesize={20} country={'in'} category={'business'} /></Route> --> Wrong Syntax for Routes*/}
+            <Route exact path="/business" element={<News key="business" pagesize={20} country={'in'} category={'business'} />} />
+            <Route exact path="/entertainment" element={<News key="entertainment" pagesize={20} country={'in'} category={'entertainment'} />} />
+            <Route exact path="/health" element={<News key="health" pagesize={20} country={'in'} category={'health'} />} />
+            <Route exact path="/science" element={<News key="science" pagesize={20} country={'in'} category={'science'} />} />
+            <Route exact path="/sports" element={<News key="sports" pagesize={20} country={'in'} category={'sports'} />} />
+            <Route exact path="/technology" element={<News key="technology" pagesize={20} country={'in'} category={'technology'} />} />
+          </Routes>
+
+        </Router>
       </div>
     )
   }
--- src/Components/News.js
+++ src/Components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
 
 export class News extends Component {
     articles = [
@@ -15,19 +16,6 @@ export class News extends Component {
             "urlToImage": "https://media.cnn.com/api/v1/images/stellar/prod/230806075838-bt15-usa-sweden-0806.jpg?c=16x9&q=w_800,c_fill",
             "publishedAt": "2023-08-06T18:06:00Z",
             "content": "The US participation in the Womens World Cup is over, ending in the cruelest of defeats. \r\nAfter a tense match, the sort which causes the stomach to churn, it is Sweden which progresses to the quarte… [+5368 chars]"
-        },
-        {
-            "source": {
-                "id": null,
-                "name": "The Guardian"
-            },
-            "author": "Guardian staff reporter",
-            "title": "Niger: thousands gather for rally to cheer generals who led coup - The Guardian",
-            "description": "Supporters wave Niger and Russia flags as Ecowas deadline for military to cede power approaches",
-            "url": "https://www.theguardian.com/world/2023/aug/06/niger-thousands-gather-for-rally-to-cheer-generals-who-led-coup",
-            "urlToImage": "https://i.guim.co.uk/img/media/0849382645aec70c405434d766b5b7201ef85c6c/0_127_4297_2579/master/4297.jpg?width=1200&height=630&quality=85&auto=format&fit=crop&overlay-align=bottom%2Cleft&overlay-width=100p&overlay-base64=L2ltZy9zdGF0aWMvb3ZlcmxheXMvdGctZGVmYXVsdC5wbmc&enable=upscale&s=553020c477efed42706d06c8a7c18c5f",
-            "publishedAt": "2023-08-06T17:36:00Z",
-            "content": "Thousands of coup supporters in Niger gathered on Sunday for a rally to cheer on the generals claiming power, as a deadline set by the west African bloc for the military to relinquish control or face… [+8724 chars]"
         }
     ]
     constructor() {
@@ -41,37 +29,43 @@ export class News extends Component {
     }
 
     async componentDidMount(){
-        let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4f08ab3e48e549cc81fdcd93d5890d2b&page=1&pagesize=20"
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=4f08ab3e48e549cc81fdcd93d5890d2b&page=1&pagesize=${this.props.pagesize}`
+        this.setState({loading:true});
         let data = await fetch(url);
         let parsedData = await data.json();
         console.log(parsedData)
         this.setState({
             articles: parsedData.articles,
-            totalResults: parsedData.totalResults
+            totalResults: parsedData.totalResults,
+            loading:false
         })
     }
 
     handlePrevClick= async () => {
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4f08ab3e48e549cc81fdcd93d5890d2b&page=${this.state.page-1}&pagesize=20`
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=4f08ab3e48e549cc81fdcd93d5890d2b&page=${this.state.page-1}&pagesize=${this.props.pagesize}`
+        this.setState({loading:true});
         let data = await fetch(url);
         let parsedData = await data.json();
         this.setState({
             page : this.state.page-1,
-            articles: parsedData.articles
+            articles: parsedData.articles,
+            loading:false
         })
     }
 
     handleNextClick= async () => {
         if(this.state.page+1 > Math.ceil(this.state.totalResults/20)){
-
+    //This if-else is used to stop next button when there is no item left to show (means disable) the next button , this statement may be deleted because I have disabled that button below and left it for learning purpose
         }
         else {
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4f08ab3e48e549cc81fdcd93d5890d2b&page=${this.state.page+1}&pagesize=20`
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=4f08ab3e48e549cc81fdcd93d5890d2b&page=${this.state.page+1}&pagesize=${this.props.pagesize}`
+        this.setState({loading:true});
         let data = await fetch(url);
         let parsedData = await data.json();
         this.setState({
             page : this.state.page+1,
-            articles: parsedData.articles
+            articles: parsedData.articles,
+            loading:false
         })
     }
     }
@@ -79,9 +73,10 @@ export class News extends Component {
     render() {
         return (
             <div className='container my-4'>
-                <h2>Top Headlines</h2>
+                <h2 className='text-center' style={{margin:"70px"}}>Top Headlines</h2>
+                { this.state.loading && <Spinner/>}
                 <div className="row my-4">
-                    {this.state.articles.map((element) => {
+                    { !this.state.loading && this.state.articles.map((element) => {
                         return <div className="col-md-3" key={element.url}>
                                 {/* <NewsItem title={element.title.slice(0, 50)} description={element.description.slice(0, 88)} newsUrl={element.url} imgUrl={element.urlToImage}/> */}
                                 <NewsItem title={element.title} description={element.description} newsUrl={element.url} imgUrl={element.urlToImage}/>
@@ -91,7 +86,7 @@ export class News extends Component {
                 </div>
                 <div className="container d-flex justify-content-between">
                 <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
-                <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+                <button disabled={this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pagesize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
                 </div>
             </div>
         )
