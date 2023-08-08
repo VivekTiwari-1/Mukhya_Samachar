7e2b7b7cb6cec24af946b38bb433c2efd394ae17
Vivek Tiwari <vivektiwarii4545@gmail.com>
2023-08-09 02:08:00 +0530

    Optimized some parts of code

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -16,12 +16,12 @@ export default class Navbar extends Component {
                                 <li className="nav-item"><Link className="nav-link " aria-current="page" to="/general">Home</Link>
                                 </li>
                                 <li className="nav-item"><Link className="nav-link " to="/business">Business</Link></li>
-                                <li className="nav-item"><Link className="nav-link " to="/entertainment">Entertainment</Link></li>
+                            
                                 <li className="nav-item"><Link className="nav-link " to="/health">Health</Link></li>
+                                <li className="nav-item"><Link className="nav-link " to="/technology">Technology</Link></li>
                                 <li className="nav-item"><Link className="nav-link " to="/science">Science</Link></li>
                                 <li className="nav-item"><Link className="nav-link " to="/sports">Sports</Link></li>
-                                <li className="nav-item"><Link className="nav-link " to="/technology">Technology</Link></li>
-                                <li className="nav-item"><Link className="nav-link " to="/general">General</Link></li>
+                                <li className="nav-item"><Link className="nav-link " to="/entertainment">Entertainment</Link></li>
                             </ul>
                             
                         </div>
